0201e29dfe36ef66d29881229388035a0779e8d9
MucaMuci <matejj.mucic@gmail.com>
2022-06-26 22:51:28 +0200

    page

---
+++ indexJS.js
@@ -0,0 +1,258 @@
+const twentyOnePilots = [
+  {
+    name: "Addict With a Pen",
+    link: "Album_Twenty_One_Pilots/Addict_with_a_pen.html",
+  },
+  {
+    name: "Fall Away",
+    link: "Album_Twenty_One_Pilots/Fall_away.html",
+  },
+  {
+    name: "The Pantaloon",
+    link: "Album_Twenty_One_Pilots/The_pantaloon.html",
+  },
+  {
+    name: "Johnny Boy",
+    link: "Album_Twenty_One_Pilots/Johnny_boy.html",
+  },
+  {
+    name: "Oh, Ms. Believer",
+    link: "Album_Twenty_One_Pilots/Oh_ms._believer.html",
+  },
+  {
+    name: "Trapdoor",
+    link: "Album_Twenty_One_Pilots/Trapdoor.html",
+  },
+  {
+    name: "A Car,A Torch, A Death",
+    link: "Album_Twenty_One_Pilots/A_car_a_torch_a_death.html",
+  },
+  {
+    name: "Isle Of Flightless Birds",
+    link: "Album_Twenty_One_Pilots/Isle_of_flightless_birds.html",
+  },
+];
+
+const regionalAtBest = [
+  {
+    name: "Guns For Hands",
+    link: "Album_Regional_At_Best/Guns_for_hands.html",
+  },
+  {
+    name: "Holding On To You",
+    link: "Album_Regional_At_Best/Holding_on_to_you.html",
+  },
+  {
+    name: "Ode To Sleep",
+    link: "Album_Regional_At_Best/Ode_to_sleep.html",
+  },
+  {
+    name: "Car Radio",
+    link: "Album_Regional_At_Best/Car_radio.html",
+  },
+  {
+    name: "Forest",
+    link: "Album_Regional_At_Best/Forest.html",
+  },
+  {
+    name: "Glowing Eyes",
+    link: "Album_Regional_At_Best/Glowing_eyes.html",
+  },
+  {
+    name: "Kitchen Sink",
+    link: "Album_Regional_At_Best/Kitchen_sink.html",
+  },
+  {
+    name: "Lovely",
+    link: "Album_Regional_At_Best/Lovely.html",
+  },
+  {
+    name: "Ruby",
+    link: "Album_Regional_At_Best/Ruby.html",
+  },
+  {
+    name: "Trees",
+    link: "Album_Regional_At_Best/Trees.html",
+  },
+  {
+    name: "House Of Gold",
+    link: "Album_Regional_At_Best/House_of_gold.html",
+  },
+];
+
+const vessel = [
+  {
+    name: "Migraine",
+    link: "Album_Vessel/Migraine.html",
+  },
+  {
+    name: "Semi Automatic",
+    link: "Album_Vessel/Semi_automatic.html",
+  },
+  {
+    name: "Screen",
+    link: "Album_Vessel/Screen.html",
+  },
+  {
+    name: "Fake You Out",
+    link: "Album_Vessel/Fake_you_out.html",
+  },
+  {
+    name: "The Run And Go",
+    link: "Album_Vessel/The_run_and_go.html",
+  },
+];
+
+const blurryface = [
+  {
+    name: "HeavyDirtySoul",
+    link: "Album_Blurryface/Heavydirtysoul.html",
+  },
+  {
+    name: "Stressed Out",
+    link: "Album_Blurryface/Stressed_out.html",
+  },
+  {
+    name: "Ride",
+    link: "Album_Blurryface/Ride.html",
+  },
+  {
+    name: "Fairly Local",
+    link: "Album_Blurryface/Fairly_local.html",
+  },
+  {
+    name: "Tear In My Heart",
+    link: "Album_Blurryface/Tear_in_my_heart.html",
+  },
+  {
+    name: "Lane Boy",
+    link: "Album_Blurryface/Lane_boy.html",
+  },
+  {
+    name: "The Judge",
+    link: "Album_Blurryface/The_judge.html",
+  },
+  {
+    name: "Doubt",
+    link: "Album_Blurryface/Doubt.html",
+  },
+  {
+    name: "Polarize",
+    link: "Album_Blurryface/Polarize.html",
+  },
+  {
+    name: "We Don't Believe What's On TV",
+    link: "Album_Blurryface/We_don't_believe_what's_on_tv.html",
+  },
+  {
+    name: "Message Man",
+    link: "Album_Blurryface/Message_man.html",
+  },
+  {
+    name: "Hometown",
+    link: "Album_Blurryface/Hometown.html",
+  },
+  {
+    name: "Not Today",
+    link: "Album_Blurryface/Not_today.html",
+  },
+  {
+    name: "Goner",
+    link: "Album_Blurryface/Goner.html",
+  },
+  {
+    name: "Heathens",
+    link: "Album_Blurryface/Heathens.html",
+  },
+];
+
+const trench = [
+  {
+    name: "Jumpsuit",
+    link: "Album_Trench/Jumpsuit.html",
+  },
+  {
+    name: "Levitate",
+    link: "Album_Trench/Levitate.html",
+  },
+  {
+    name: "Morph",
+    link: "Album_Trench/Morph.html",
+  },
+  {
+    name: "My Blood",
+    link: "Album_Trench/My_blood.html",
+  },
+  {
+    name: "Chlorine",
+    link: "Album_Trench/Chlorine.html",
+  },
+  {
+    name: "Smithereens",
+    link: "Album_Trench/Smithereens.html",
+  },
+  {
+    name: "Neon Gravestones",
+    link: "Album_Trench/Neon_gravestones.html",
+  },
+  {
+    name: "The Hype",
+    link: "Album_Trench/The_hype.html",
+  },
+  {
+    name: "Nico And The Niners",
+    link: "Album_Trench/Nico_and_the_niners.html",
+  },
+  {
+    name: "Cut My Lip",
+    link: "Album_Trench/Cut_my_lip.html",
+  },
+  {
+    name: "Bandito",
+    link: "Album_Trench/Bandito.html",
+  },
+  {
+    name: "Pet Cheetah",
+    link: "Album_Trench/Pet_cheetah.html",
+  },
+  {
+    name: "Legend",
+    link: "Album_Trench/Legend.html",
+  },
+  {
+    name: "Leave The City",
+    link: "Album_Trench/Leave_the_city.html",
+  },
+];
+
+const singles = [
+  {
+    name: "Level Of Concern",
+    link: "Album_Singles/Level_of_concern.html",
+  },
+  {
+    name: "Cancer",
+    link: "Album_Singles/Cancer.html",
+  },
+  {
+    name: "Can't Help Falling In Love With You",
+    link: "Album_Singles/Can't_help_falling_in_love_with_you.html",
+  },
+];
+
+function printAlbumSongs(list, divId) {
+  list.map((song) => {
+    const songLink = document.createElement("a");
+    const br = document.createElement("br");
+
+    let linkText = document.createTextNode(song.name);
+    songLink.appendChild(linkText);
+
+    songLink.href = song.link;
+    songLink.target = "_blank";
+
+    const element = document.getElementById(divId);
+    element.appendChild(songLink);
+    element.appendChild(br);
+  });
+}
